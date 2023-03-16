94f7da7fa0a2f7632d19a56b615bb390c016ac44
KimaniSarah <bibokimani@gmail.com>
2023-03-16 06:03:22 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - print "Programming is like building a multilingual puzzle"
+ *
+ * return 0
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a miltilingual puzzle"
+		puts(str);
+	return (0);
+}
