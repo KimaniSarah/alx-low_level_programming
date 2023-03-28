15554fb9898297c8e3e7227f338295eb45d67bb4
KimaniSarah <bibokimani@gmail.com>
2023-03-28 01:55:19 -0700

    Half the lies they tell about me are not true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2
+ * @str: pointer input
+ *
+ * Return: string
+ */
+void puts2(char *str)
+{
+	int j;
+	int length = strlen(str);
+
+	for (j = 0; j < length; j += 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
