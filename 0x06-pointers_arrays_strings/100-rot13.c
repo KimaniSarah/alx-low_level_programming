911adadb22a6e7cbb67332f8bd2825437eb53b09
KimaniSarah <bibokimani@gmail.com>
2023-03-31 00:07:38 -0700

    rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * rot13 - encodes input string
+ * @s: the input string to be encoded
+ *
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char *upper_half = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *lower_half = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			if (s[i] == upper_half[j])
+			{
+				s[i] = lower_half[j];
+				break;
+			}
+			else if (s[i] == lower_half[j])
+			{
+				s[i] = upper_half[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
