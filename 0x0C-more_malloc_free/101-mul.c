becd8750913db49f0ea479285854a8c6d819db32
KimaniSarah <bibokimani@gmail.com>
2023-04-14 03:16:42 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _atoi - convert a string into an integer.
+ *
+ * @s: the string to use.
+ *
+ * Return: integer.
+ */
+
+int _atoi(char *s)
+{
+	int h = 1;
+	int x = 0;
+	unsigned int v = 0;
+
+	while (!(s[x] <= '9' && s[x] >= '0') && s[x] != '\0')
+	{
+		if (s[x] == '-')
+			h *= -1;
+		x++;
+	}
+	while (s[x] <= '9' && (s[x] >= '0' && s[x] != '\0'))
+	{
+		v = (v * 10) + (s[x] - '0');
+		x++;
+	}
+	v *= h;
+	return (v);
+}
+/**
+ *  _isdigit - identify digits
+ *  @c: digit to be identified
+ *
+ *  Return: 1 if c is a digit 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
 /**
  * main - multiply 2 positive numbers
  * @argc:the number of arguments
@@ -18,21 +63,21 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
 	for (h = 1; h < argc; h++)
 	{
 		for (v = 0; argv[h][v] != '\0'; v++)
 		{
-			if (!isdigit(argv[h][v]))
+			if (!_isdigit(argv[h][v]))
 			{
 				printf("Error\n");
-				exit(98);
+				return (98);
 			}
 		}
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 	multiply = num1 * num2;
 
 	printf("%d\n", multiply);
