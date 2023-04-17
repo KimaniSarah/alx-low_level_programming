f43e20a0148d060ebe2263f41dd5e04fcda7405a
KimaniSarah <bibokimani@gmail.com>
2023-04-17 06:06:01 -0700

    How many legs does a dog have if you call his tail a leg?

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include"dog.h"
+/**
+ * free_dog -  frees dogs
+ * @d:the dogs to be freed
+ *
+ * Return:void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+		if (d->owner != NULL)
+		{
+			free(d->owner);
+		}
+		free(d);
+	}
+}
