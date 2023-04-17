84bf738b00796e3d49b6325ba7d39bedd57966ff
KimaniSarah <bibokimani@gmail.com>
2023-04-17 02:50:23 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+/**
+ * struct dog - structure
+ * @name:dog name
+ * @age:dog age
+ * @owner:dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
