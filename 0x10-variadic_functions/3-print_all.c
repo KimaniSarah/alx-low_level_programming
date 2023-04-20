5e4d405d49bdec19da713bf66339f24325511111
KimaniSarah <bibokimani@gmail.com>
2023-04-20 13:20:35 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,6 @@
 #include <stdarg.h>
-#include "variadic_functions.h"
 #include <stdio.h>
+
 /**
  * print_char - prints a character
  * @arg: argument to print
@@ -9,6 +9,7 @@ void print_char(va_list arg)
 {
 	printf("%c", va_arg(arg, int));
 }
+
 /**
  * print_int - prints an integer
  * @arg: argument to print
@@ -26,6 +27,7 @@ void print_float(va_list arg)
 {
 	printf("%f", va_arg(arg, double));
 }
+
 /**
  * print_str - prints a string
  * @arg: argument to print
@@ -39,6 +41,7 @@ void print_str(va_list arg)
 	else
 		printf("%s", str);
 }
+
 /**
  * print_all - prints anything
  * @format: format string containing the types of the arguments
@@ -46,31 +49,33 @@ void print_str(va_list arg)
 void print_all(const char * const format, ...)
 {
 	va_list arg;
-	int h = 0, v;
+	int i = 0, j = 0;
 	char *separator = "";
 	char *types = "cifs";
 	void (*print_fn[])(va_list) = {print_char, print_int, print_float, print_str};
 
 	va_start(arg, format);
-	while (format && format[h])
-	{
-		v = 0;
 
-		while (types[v])
+	while (format && format[i])
+	{
+		j = 0;
+
+		while (types[j])
 		{
-			if (types[v] == format[h])
+			if (types[j] == format[i])
 			{
 				printf("%s", separator);
-				print_fn[v](arg);
+				print_fn[j](arg);
 				separator = ", ";
 			}
 
-			v++;
+			j++;
 		}
 
-		h++;
+		i++;
 	}
 
 	printf("\n");
 	va_end(arg);
 }
+
