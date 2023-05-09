4679a25b195cfa9c970740a85c8ba49833065541
KimaniSarah <bibokimani@gmailcom>
2023-05-09 00:49:18 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,51 @@
 #include "main.h"
 /**
- * read_textfile - reads a text file and prints it to the POSIX std output.
- * @filename: file containing letters to be read and written
- * @letters:number of letters it should read and print
- * Return:actual number of letters it could read and print
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ * Return: actual number of letters read and printed, or 0 if an error occurred
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buffer[1024];
-	ssize_t total_bytes_read;
-	ssize_t bytes_read;
-	ssize_t bytes_to_write;
-	ssize_t bytes_written;
 	int fd;
+	ssize_t total_bytes_read = 0, bytes_read, bytes_written;
+	char *buf;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
 
-	fd = read(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 	{
+		close(fd);
 		return (0);
 	}
-	total_bytes_read = 0;
-	while (total_bytes_read < letters &&
-			(bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
-	{
-		bytes_to_write = bytes_read;
-		if (total_bytes_read + bytes_to_write > letters)
-		{
-			bytes_to_write = letters - total_bytes_read;
-		}
-		bytes_written = write(STDOUT_FILENO, buffer, bytes_to_write);
-		if (bytes_written != bytes_to_write)
-	{
-			close(fd);
-			return (0);
-		}
 
-		total_bytes_read += bytes_written;
+	bytes_read = read(fd, buf, letters);
+	if (bytes_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
 	}
 
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	total_bytes_read += bytes_read;
+
+	free(buf);
 	close(fd);
+
 	return (total_bytes_read);
 }
+
