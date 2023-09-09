775c87325061f9b705facb6155a012f301fa7962
KimaniSarah <bibokimani@gmailcom>
2023-09-09 00:03:11 -0700

    delete has table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -17,7 +17,7 @@ void hash_table_delete(hash_table_t *ht)
 			while (current != NULL)
 			{
 				temp = current->next;
-				free(current>key);
+				free(current->key);
 				free(current->value);
 				free(current);
 				current = temp;
